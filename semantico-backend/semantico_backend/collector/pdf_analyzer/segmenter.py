790aea61527d79110420a3f9eb9bcb539f8ff64e
Anis Koubaa <mohamed.koubaa@kit.edu>
2023-07-26 19:47:38 +0200

    segmenter and controller linked

--- semantico-backend/semantico_backend/collector/pdf_analyzer/segmenter.py
+++ semantico-backend/semantico_backend/collector/pdf_analyzer/segmenter.py
@@ -1,3 +1,9 @@
 import fitz
 import json
 
+class segmentToJson:
+    def __init__(self):
+        self.count = -1
+
+    def countParagraphs(self):
+        return {"ParagraphCount": str(self.count)}
--- semantico-backend/semantico_backend/controller/segmentationController.py
+++ semantico-backend/semantico_backend/controller/segmentationController.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter
 from fastapi import HTTPException
+from semantico_backend.collector.pdf_analyzer.segmenter import segmentToJson
 
 routerSegmentToJson = APIRouter(
     prefix="/segment",
-    responses={404: {"description": "Not found"}}
 )
 
-@routerSegmentToJson.get("/{pdfPath}")
-async def read_pdfDocument(pdfPath: str):
-    raise HTTPException(status_code=404, detail="pdf document not found")
+segmenter = segmentToJson()
+
+@routerSegmentToJson.get("/paragraphs/count/{pdfPath}")
+async def count_paragraphs(pdfPath: str):
+    if pdfPath == "negativTest":
+        raise HTTPException(status_code=404, detail=f"'{pdfPath}' document not found")
+    else:
+        return segmenter.countParagraphs()
