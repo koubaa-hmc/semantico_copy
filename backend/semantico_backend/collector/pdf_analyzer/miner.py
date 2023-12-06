43d5611fe00e796dc268c0808298bd6c234d07b4
Anis Koubaa <mohamed.koubaa@kit.edu>
2023-12-06 15:51:09 +0100

    pass to miner six.

--- backend/semantico_backend/collector/pdf_analyzer/miner.py
+++ backend/semantico_backend/collector/pdf_analyzer/miner.py
@@ -1,9 +1,4 @@
-from pdfminer.converter import PDFPageAggregator
-from pdfminer.layout import LAParams, LTFigure, LTTextBox
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
-from pdfminer.pdfpage import PDFPage, PDFTextExtractionNotAllowed
-from pdfminer.pdfparser import PDFParser
+from pdfminer.high_level import extract_text
 
 
 class PdfReader:
@@ -11,18 +6,4 @@ class PdfReader:
         self.path = path
 
     def extract_text(self):
-        with open(self.path, 'rb') as f:
-            parser = PDFParser(f)
-            doc = PDFDocument(parser)
-            page = list(PDFPage.create_pages(doc))[0]
-            rsrcmgr = PDFResourceManager()
-            device = PDFPageAggregator(rsrcmgr, laparams=LAParams())
-            interpreter = PDFPageInterpreter(rsrcmgr, device)
-            interpreter.process_page(page)
-            layout = device.get_result()
-            text = ""
-            for obj in layout:
-                if isinstance(obj, LTTextBox):
-                    text += obj.get_text()
-
-        return text
+        return extract_text(self.path)
